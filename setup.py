66c008a6fbeb25b5957e685fb00f4789dea45ef5
Smoke Jaguar <tanc7@unlv.nevada.edu>
2017-07-10 17:45:04 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+import socket
+import sys
+import operator
+
+# install Tor, Tsocks, and NMap for you
+
+os.system("sudo apt-get update && apt-get install tor tsocks nmap")
+
+# git clone the latest repo
+os.chdir("/tmp")
+os.system("sudo git clone https://raw.githubusercontent.com/tanc7/Py_Vegas_Demo/")
+
+# change to directory and modify file permissions
+os.chdir("/tmp/Py_Vegas_Demo")
+os.system("sudo chmod 777 nmap_auto_script_demo.py")
+
+# Make a installation directory and copy the modified file over
+os.system("mkdir /root/Documents/nmap_auto_script_project/")
+os.system("cp -r nmap_auto_script_demo.py /root/Documents/nmap_auto_script_project/nmap_auto_script_demo.py")
+
+# automatically start the copy
+os.system("python /root/Documents/nmap_auto_script_project/nmap_auto_script_demo.py")
